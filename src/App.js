a257277bad961bb3bb62d59eca7bcd95bc3977d0
Geert van der Ploeg <geert.van.der.ploeg@finalist.nl>
2016-09-26 10:11:35 +0200

    Get a request working, more or less

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+      title: 'initial'
+    };
+  }
+
+
+  componentDidMount() {
+    this.serverRequest = fetch(this.props.source, {
+      mode: 'no-cors'
+    })
+    .then(function(response) {
+      this.setState({
+        title: "" + response.data
+      })
+      
+    }.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.serverRequest.abort();
+  }
+
+
   render() {
-    return (
-      <div>Hello world</div>
-    );
+      return (
+         <div>
+        Title is: {this.state.title}</div>
+      );
+    
   }
 }
 
